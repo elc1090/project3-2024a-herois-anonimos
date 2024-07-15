da28072cceeb3c2d139f089be8445df7afd06924
Wederson Fagundes <wedymf@hotmail.com>
2024-07-15 05:26:52 -0300

    style: fix width

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -13,14 +13,14 @@ export default async function DashboardPage() {
             key={index}
             className="bg-white/40 border animate-pulse border-slate-300 px-4 py-2 h-20 rounded shadow flex justify-between items-center"
           >
-            <div className="flex flex-col gap-1 w-full">
-              <div className="bg-slate-300 h-5 w-96" />
+            <div className="flex flex-col gap-1 w-72 lg:w-96">
+              <div className="bg-slate-300 h-5 w-full" />
               <div className="h-[14px] bg-slate-300 w-60" />
             </div>
 
-            <div className="flex gap-4">
-              <div className="w-[105px] h-10 bg-slate-300 rounded-sm" />
-              <div className="w-[105px] h-10 bg-slate-300 rounded-sm" />
+            <div className="flex gap-2 lg:gap-4 flex-nowrap">
+              <div className=" w-10 lg:w-[105px] h-10 bg-slate-300 rounded-sm" />
+              <div className=" w-10 lg:w-[105px] h-10 bg-slate-300 rounded-sm" />
             </div>
           </article>
         ))}
